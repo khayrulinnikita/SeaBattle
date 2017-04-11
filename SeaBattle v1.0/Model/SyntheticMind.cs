ea65e0771d996afc4794ffd905c32ac0505b3836
khayrulinnikita <khayrulinnikita@gmail.com>
2017-04-12 01:06:28 +0300

    new array

---
+++ SeaBattle v1.0/Model/SyntheticMind.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle_v1._0.Model
+{
+    class SyntheticMind
+    {
+        public void Fire(int[,] StateCell)
+        {
+
+
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write(StateCell[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
